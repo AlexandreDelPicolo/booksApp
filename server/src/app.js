135ae715f350a736f3f0e454e7c1338405f4d920
Alexandre Del Picolo <adpicolo@voegol.com.br>
2020-10-14 20:02:24 -0300

    Adicionada a lib mongoose e implementada a conexão com o mongodb.

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
 
+// Database connection
+mongoose.connect('mongodb://localhost:27017/graphql-study', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('connected to database localhost:27017');
+});
+
+// Middleware graphql
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
